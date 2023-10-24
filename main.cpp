f0e880fd3f4dc6efd8bcc67ea9700a371b427467
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-24 20:43:41 +0300

    ex - 2 was done

--- main.cpp
+++ main.cpp
@@ -3,5 +3,31 @@
 using namespace std;
 
 int main() {
+	int n;
 	
+	cout << "Vedit chuslo: ";
+	cin >> n;
+
+	const int rows = 3;
+	const int cols = n;
+
+	int array[rows][cols];
+
+	int value = n;
+	for(int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			array[i][j] = value;
+			value++;
+		}
+
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			cout << array[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
